1d21869a6ab2cb0bd2489c0d765cc8801a03d7f9
Ayush Raje <74646306+Ayushcoder770@users.noreply.github.com>
2020-11-23 18:29:39 +0530

    Create new.cpp

---
+++ new.cpp
@@ -0,0 +1,15 @@
+//this is a code
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+int n=0,i;
+cout<<"enter a num:";
+cin>>n;
+for(i=1;i<=10;i++)
+   {
+   cout<<n<<"*"<<i<<"="<<n*i;
+   }
+return(0);
+}

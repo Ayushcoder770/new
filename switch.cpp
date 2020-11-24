e103111bb5d92c32f8bd4f6f6e213d321b90086c
Tejasv Chak <tejasvchak@Tejasvs-MacBook-Pro.local>
2020-11-24 15:49:19 +0530

    added switch

---
+++ switch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ using namespace std;
+ int main()
+ {int a;
+ cout<<"enter"<<endl;
+ cin>>a;
+ switch(a)
+    {case1:
+    cout<<"excellent";
+    break;
+    
+    case2: cout<<"good";
+    break;
+
+    default: cout<<"bad";
+    break;
+    }
+return(0);
+ }
